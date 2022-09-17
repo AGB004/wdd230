c70c4dfb09bab1c8725025cd8034f914e71fcec4
AGB004 <alecbyington@gmail.com>
2022-09-17 12:37:53 -0600

    added last updated date

---
+++ script/script.js
@@ -0,0 +1,8 @@
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+    year: "numeric"
+};
+
+document.getElementById("lastupdate").textContent = new Date().toLocaleDateString("en-US, options");
