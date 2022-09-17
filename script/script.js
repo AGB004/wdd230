fb4d8a6f9f72b3855d2a827f6cf4754165c2b5a7
AGB004 <alecbyington@gmail.com>
2022-09-17 13:19:44 -0600

    update js

--- script/script.js
+++ script/script.js
@@ -7,5 +7,37 @@ const options = {
     year: "numeric"
 };
 
-/* document.getElementById("lastupdate").textContent = new Date().toLocaleDateString("en-US, options");*/
-lastupdate.textContent = new Date().toLocaleDateString("en-US, options");
+// document.getElementById("lastupdate").textContent = new Date().toLocaleDateString("en-US, options");
+// lastupdate.textContent = new Date().toLocaleDateString("en-US, options");
+
+// Long hand method ... building day and month names from built-in date methods.
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+lastupdate.textContent = fulldate;
