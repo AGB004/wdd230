fccb4462c3740d6feb6349bf8f3282e783ff7104
AGB004 <alecbyington@gmail.com>
2022-09-19 17:43:43 -0600

    long js

--- script/script.js
+++ script/script.js
@@ -1,11 +1,6 @@
 let lastupdate = document.querySelector("#lastupdate");
 
-const options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric"
-};
+//const options = {weekday: "long",day: "numeric",month: "long",year: "numeric"};
 
 // document.getElementById("lastupdate").textContent = new Date().toLocaleDateString("en-US, options");
 // lastupdate.textContent = new Date().toLocaleDateString("en-US, options");
