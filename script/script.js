d371c745f37ccbb11cb27f5af626496974e9994e
AGB004 <alecbyington@gmail.com>
2022-10-01 22:49:40 -0600

    setting up site_plan draft

--- script/script.js
+++ script/script.js
@@ -1,38 +1,41 @@
-let lastupdate = document.querySelector("#lastupdate");
+// let lastupdate = document.querySelector("#lastupdate");
 
-//const options = {weekday: "long",day: "numeric",month: "long",year: "numeric"};
+// //const options = {weekday: "long",day: "numeric",month: "long",year: "numeric"};
 
-// document.getElementById("lastupdate").textContent = new Date().toLocaleDateString("en-US, options");
-// lastupdate.textContent = new Date().toLocaleDateString("en-US, options");
+// // document.getElementById("lastupdate").textContent = new Date().toLocaleDateString("en-US, options");
+// // lastupdate.textContent = new Date().toLocaleDateString("en-US, options");
 
-// Long hand method ... building day and month names from built-in date methods.
+// // Long hand method ... building day and month names from built-in date methods.
 
-const daynames = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-const months = [
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-];
-const d = new Date();
-const dayName = daynames[d.getDay()];
-const monthName = months[d.getMonth()];
-const year = d.getFullYear();
-const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
-lastupdate.textContent = fulldate;
+// const daynames = [
+// 	"Sunday",
+// 	"Monday",
+// 	"Tuesday",
+// 	"Wednesday",
+// 	"Thursday",
+// 	"Friday",
+// 	"Saturday"
+// ];
+// const months = [
+// 	"January",
+// 	"February",
+// 	"March",
+// 	"April",
+// 	"May",
+// 	"June",
+// 	"July",
+// 	"August",
+// 	"September",
+// 	"October",
+// 	"November",
+// 	"December"
+// ];
+// const d = new Date();
+// const dayName = daynames[d.getDay()];
+// const monthName = months[d.getMonth()];
+// const year = d.getFullYear();
+// const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+// lastupdate.textContent = fulldate;
+
+//document.querySelector('#year').textContent = new Date().getFullYear();
+document.getElementById('lastupdate').textContent = document.lastModified
