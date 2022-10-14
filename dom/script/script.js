36139f8ea32a582050b3c768968506deebba84ed
AGB004 <alecbyington@gmail.com>
2022-10-13 23:51:00 -0600

    update js with comments

--- dom/script/script.js
+++ dom/script/script.js
@@ -3,31 +3,45 @@ const input = document.querySelector("input");
 const button = document.querySelector("button");
 const list = document.querySelector("ul");
 
-
+// Create an click event listener for the Add Chapter button using addEventListener and an anonymous function.
 button.addEventListener("click", function() {
+    // In the function block for adding a chapter, make sure the input is not blank
     if (input.value.length === 0) {
+        // change placeholder to remind user to input value
         document.getElementById("favchap").placeholder = "Please insert text, EX: Alma 5";
     } 
-    
+    //  and if not, do the following:
     else {
+        // return placeholder to original
         document.getElementById("favchap").placeholder = "Alma 5";
 
-        const listItem = document.createElement("li");
-        const listText = document.createElement("span");
-        const listBtn = document.createElement("button");
+        // a. b. Create three variables that hold references to create li, button, and span elements using const.
+        const list_item = document.createElement("li");
+        const input_text = document.createElement("span");
+        const delete_button = document.createElement("button");
 
-        listItem.appendChild(listText);
-        listText.textContent = input.value;
-        listItem.appendChild(listBtn);
-        listBtn.textContent = "❌";
+        // c. populate the li elements textContent or innerHTML with the input
+        input_text.textContent = input.value;
 
-        list.appendChild(listItem);
+        // d. populate the button textContent with an ❌
+        delete_button.textContent = "❌";
 
-        input.value = "";
+        // e. append the li element with the delete button
+        list_item.appendChild(input_text);
+        list_item.appendChild(delete_button);
 
-        listBtn.addEventListener("click", function() {
-            list.removeChild(listItem);
+        // f. append the list element with the li element just created and appended with text and the delete button
+        list.appendChild(list_item);
+
+        // g. add an event listener to the delete button that removes the li element when clicked
+        delete_button.addEventListener("click", function() {
+            list.removeChild(list_item);
         });
     };
+
+    // h. send the focus to the input element
     input.focus();
+
+    // i. clean up the successful add of a chapter by changing the input to nothing or the empty string and setting the focus to the input.
+    input.value = "";
 });
