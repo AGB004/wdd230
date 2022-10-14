782f8f2ea56f116150d9428e89c47db30a756c35
AGB004 <alecbyington@gmail.com>
2022-10-13 23:10:12 -0600

    update bom js

--- dom/script/script.js
+++ dom/script/script.js
@@ -30,31 +30,3 @@ button.addEventListener("click", function() {
     };
     input.focus();
 });
-// In the function block for adding a chapter, make sure the input is not blank and if not, do the following:
-
-// create an li element
-
-// create a delete button
-
-// populate the li elements textContent or innerHTML with the input
-
-// populate the button textContent with an ❌
-
-// append the li element with the delete button
-
-// append the list element with the li element just created and appended with text and the delete button
-
-// add an event listener to the delete button that removes the li element when clicked
-
-// send the focus to the input element
-
-// clean up the successful add of a chapter by changing the input to nothing or the empty string and setting the focus to the input.
-
-
-// Create an click event listener for the Add Chapter button using addEventListener and an anonymous function.  
-// button.addEventListener('click', function() { ...
-
-
-
-
-
