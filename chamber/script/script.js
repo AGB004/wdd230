447f9ceee3b6790147b08504fe7e89c7c170db50
AGB004 <alecbyington@gmail.com>
2022-11-12 11:09:39 -0700

    update json, directory css

--- chamber/script/script.js
+++ chamber/script/script.js
@@ -93,40 +93,35 @@ fetch('chamber/json/data.json')
     return response.json();
   })
   .then(function (jsonObject) {
+    console.table(jsonObject);  // temporary checking for valid response and data parsing
     const businesses = jsonObject['businesses'];
     businesses.forEach(displayBusinesses);
   });
 
 function displayBusinesses(business) {
-  // Create elements to add to the document
-  let card = document.createElement('div');
+  let card = document.createElement('section');
   let image = document.createElement('img');
   let h2 = document.createElement('h2');
   let address = document.createElement('p');
   let phoneNumber = document.createElement('p');
   let website = document.createElement('a');
 
-  // Change the textContent property of the h2 element to contain the prophet's full name
   h2.textContent = business.name;
   address.textContent = `Address: ${business.address}`;
   phoneNumber.textContent = `Contact: ${business.phoneNumber}`;
   website.textContent = business.webURL;
   website.href = business.webURL;
 
-
-  // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
   image.setAttribute('src', business.image);
   image.setAttribute('alt', `${business.name}`);
   image.setAttribute('loading', 'lazy');
 
-  // Add/append the section(card) with the h2 element
   card.appendChild(h2);
   card.appendChild(image);
   card.appendChild(address);
   card.appendChild(phoneNumber);
   card.appendChild(website);
 
-  // Add/append the existing HTML div with the cards class with the section(card)
   const grid = document.querySelector('.grid');
   grid.appendChild(card);
 }
