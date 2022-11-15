b240590842491b81d325caafc3729fc0287b1c0f
AGB004 <alecbyington@gmail.com>
2022-11-14 17:45:51 -0700

    update pics, js

--- chamber/script/script.js
+++ chamber/script/script.js
@@ -48,7 +48,7 @@ const banner = document.querySelector('#greet');
 if (today === 1 || today === 2) {
   join_banner = document.createElement('p');
   join_banner.innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-  banner.append(join_banner)
+  banner.append(join_banner);
 };
 
 // ----- Lazy Load ------ //
