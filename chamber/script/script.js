41f1275e39022cc24dcb4a4e0e5bc832a1f135d7
AGB004 <alecbyington@gmail.com>
2022-11-15 21:26:09 -0700

    update banner js, add lesson10

--- chamber/script/script.js
+++ chamber/script/script.js
@@ -41,16 +41,6 @@ function toggleMenu() {
 const x = document.getElementById("hamburgerBtn");
 x.onclick = toggleMenu;
 
-// ----- Chamber Meeting ------ //
-const today = d.getDay();
-const banner = document.querySelector('#greet');
-
-if (today === 1 || today === 2) {
-  join_banner = document.createElement('p');
-  join_banner.innerHTML = "🤝🏼 Come join us for the chamber meet and greet Wednesday at 7:00 p.m.";
-  banner.append(join_banner);
-};
-
 // ----- Lazy Load ------ //
 let imagesToLoad = document.querySelectorAll("img[data-src]");
 
